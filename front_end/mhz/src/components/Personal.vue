f22c9779658a44cc34e4d8d5e9b4bbe9f1ed4872
menghuiZ <32238574+menghuiZ@users.noreply.github.com>
2018-07-07 20:40:25 +0800

    Add files via upload

---
+++ front_end/mhz/src/components/Personal.vue
@@ -0,0 +1,284 @@
+<template>
+    <div>
+        <div class="cover">
+            <div class="cover-content">
+                <mu-avatar class="avatar">
+                    <img src="../assets/avatar.jpg">
+                </mu-avatar>
+                <div class="txt">
+                    <p class="padding">{{ name }}</p>
+                </div>
+            </div>
+        </div>
+        <div class="main-content">
+            <el-row class="tac">
+                <el-col :span="24" class="left">
+                    <div v-if="bottomNav == '0'">
+                        <el-table :data="tableData" stripe class="center">
+                            <el-table-column prop="label"></el-table-column>
+                            <el-table-column prop="value"></el-table-column>
+                        </el-table>
+                    </div>
+                    <div v-if="bottomNav == '1'" v-for="activity in orgAtyArr" class="container">
+                        <mu-card raised>
+                            <mu-card-media>
+                                <img src="../assets/bg.jpg">
+                            </mu-card-media>
+                            <span class="card-title">{{ activity.name }}</span>
+                            <div class="location">
+                                <i class="fas fa-map-marker-alt"></i>
+                                <div>{{ activity.place }}</div>
+                            </div>
+                            <mu-card-actions>
+                                <mu-button class="muBtn" flat large @click="detailActivity(activity)">详情</mu-button>
+                            </mu-card-actions>
+                        </mu-card>
+                    </div>
+                    <div v-if="bottomNav == '2'" v-for="activity in tkinAtyArr" class="container">
+                        <mu-card raised>
+                            <mu-card-media>
+                                <img src="../assets/bg.jpg">
+                            </mu-card-media>
+                            <span class="card-title">{{ activity.name }}</span>
+                            <div class="location">
+                                <i class="fas fa-map-marker-alt"></i>
+                                <div>{{ activity.place }}</div>
+                            </div>
+                            <mu-card-actions>
+                                <mu-button class="muBtn" flat large @click="detailActivity(activity)">详情</mu-button>
+                            </mu-card-actions>
+                        </mu-card>
+                    </div>
+                    <div v-if="bottomNav == '3'">{{toAllActivity()}}</div>
+                    <div v-if="bottomNav == '4'">{{toCreate()}}</div>
+                </el-col>
+            </el-row>
+            <mu-bottom-nav :value="bottomNav" shift @change="handleChange" class="bottom-nav">
+                <mu-bottom-nav-item value="0" title="个人中心" icon="perm_identity"/>
+                <mu-bottom-nav-item value="1" title="组织活动" icon="star_rate"/>
+                <mu-bottom-nav-item value="2" title="参加活动" icon="pan_tool"/>
+                <mu-bottom-nav-item value="3" title="查看活动" icon="visibility"/>
+                <mu-bottom-nav-item value="4" title="创建活动" icon="add"/>
+            </mu-bottom-nav>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Personal',
+
+    data() {
+        return {
+            bottomNav: '0',
+            uid: '',
+            name: '',
+            tableData: [],
+            orgAtyArr: [],
+            tkinAtyArr: [],
+        }
+    },
+
+    created() {
+        if (this.$store.state.isLogin == "") {
+          this.$router.push('/signin')
+      }
+        if (this.$route.params.bottomNav)
+            this.bottomNav = this.$route.params.bottomNav
+        this.$route.params.uid = this.$store.state.isLogin
+        this.fetchData()
+        
+    },
+
+    methods: {
+        fetchData() {
+            this.getUser()
+            this.getOrganizeActivity()
+            this.getTakeinActivity()
+        },
+        handleChange(val) {
+            this.bottomNav = val
+        },
+        toAllActivity() {
+            this.$router.push({name: 'Activity', params: {uid: this.$route.params.uid, bottomNav: this.bottomNav}})
+        },
+        toCreate() {
+            this.$router.push({name: 'Create', params: {uid: this.$route.params.uid, bottomNav: this.bottomNav}})
+        },
+        detailActivity(activity) {
+            this.$router.push({name: 'Detail', params: {uid: this.$route.params.uid, aid: activity.id, bottomNav: this.bottomNav}})
+        },
+
+        getUser() {
+            this.$ajax
+                .get('/api/getOneUser?id=' + this.$route.params.uid)
+                .then(res => {
+                    if (res.data.ok) {
+                        this.tableData = []
+                        let user = res.data.data
+                        this.tableData.push({label: '姓名', value: user.name})
+                        this.tableData.push({label: '学号', value: user.id})
+                        this.tableData.push({label: '手机', value: user.phone})
+                        this.tableData.push({label: '邮箱', value: user.mail})
+                        this.uid = user.id
+                        this.name = user.name
+                    }
+                    else
+                        alert('fail to get user')
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        },
+        getOrganizeActivity() {
+            this.$ajax
+                .get('/api/getOneUserOrganize?uid=' + this.$route.params.uid)
+                .then((res) => {
+                    let activityID = res.data.data
+                    if (activityID.length != 0) {
+                        this.orgAtyArr = []
+                        activityID.forEach(id => {
+                            this.$ajax
+                                .get('/api/getOneActivity?id=' + id)
+                                .then(res => {
+                                    this.orgAtyArr.push(res.data.data)
+                                })
+                                .catch(err => {
+                                    console.log(err)
+                                })
+                        })
+                    }
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        },
+        getTakeinActivity() {
+            this.$ajax
+                .get('/api/getOneUserTakein?uid=' + this.$route.params.uid)
+                .then((res) => {
+                    let activityID = res.data.data
+                    if (activityID.length != 0) {
+                        this.tkinAtyArr = []
+                        activityID.forEach(id => {
+                            this.$ajax
+                                .get('/api/getOneActivity?id=' + id)
+                                .then(res => {
+                                    this.tkinAtyArr.push(res.data.data)
+                                })
+                                .catch(err => {
+                                    console.log(err)
+                                })
+                        })
+                    }
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        },
+    },
+}
+</script>
+
+<style scoped>
+.personal {
+    height: 100vh;
+}
+
+.avatar {
+    margin-top:20px;
+    margin-bottom: 10px;
+    width: 200px;
+    height: 200px;
+}
+
+.container {
+    margin-bottom: 5px;
+    padding: 30px;
+}
+
+.bottom-nav {
+    position: fixed;
+    bottom: 0;
+}
+
+.center {
+    text-align: center;
+}
+
+.left {
+    text-align: left;
+}
+
+span {
+    display: inline;
+    margin-left: 5px;
+    vertical-align: top;
+}
+
+.detail-button {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    width: 30px;
+    height: 30px;
+}
+
+i {
+    display: inline;
+    vertical-align: baseline;
+}
+
+p {
+    display: inline;
+    line-height: 30px;
+    margin-left: 5px;
+    vertical-align: top;
+}
+
+.txt {
+    margin-top: 20px;
+    margin-left: 10px;
+    height: 30px;
+    font-size: 60px;
+    color: white;
+    font-weight: bold;
+}
+
+.cover {
+    background-color: #545c64;
+    height: 400px;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    z-index: 5;
+}
+.cover-content {
+    padding-top: 50%;
+    transform: translateY(-50%)
+}
+.main-content {
+    margin-top: 400px;
+    z-index: 4;
+    padding-bottom: 110px;
+}
+.mu-card {
+    border-radius: 16px;
+    overflow: hidden;
+}
+.card-title {
+    font-size: 60px;
+    margin-left: 20px;
+    padding-right: 40px;
+    border-bottom: 1px solid gray;
+}
+.location {
+    margin-left: 20px;
+    font-size: 30px;
+    display: flex;
+    align-items: center;
+}
+.fa-map-marker-alt {
+    margin-right: 10px;
+}
+</style>
---
+++ front_end/mhz/src/components/Signup.vue
@@ -0,0 +1,200 @@
+<template>
+    <div>
+        <img class="logo" src='../assets/logo.jpg'/>
+        <div class="container">
+            <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm">
+                <mu-text-field label-float
+                  prop="username"
+                  label="姓名"
+                  v-model="ruleForm2.username">
+                </mu-text-field>
+                <mu-text-field label-float
+                  prop="sid"
+                  label="学号"
+                  v-model.number="ruleForm2.sid">
+                </mu-text-field>
+                <mu-text-field label-float
+                  prop="pass"
+                  label="密码"
+                  type="password" 
+                  v-model="ruleForm2.pass">
+                </mu-text-field>
+                <mu-text-field label-float
+                  prop="checkPass"
+                  label="确认密码"
+                  type="password" 
+                  v-model="ruleForm2.checkPass">
+                </mu-text-field>
+                <mu-text-field label-float
+                  prop="phone"
+                  label="手机"
+                  v-model.number="ruleForm2.phone">
+                </mu-text-field>
+                <mu-text-field label-float
+                  prop="email"
+                  label="邮箱"
+                  v-model="ruleForm2.email">
+                </mu-text-field><br />
+                <el-button type="primary" @click="submitForm('ruleForm2')">注册</el-button>
+                <el-button @click="login">登录</el-button>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Signup',
+  
+  data() {
+      let validateUsername = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入姓名'));
+        }
+        else {
+          callback();
+        }
+      };
+      let validateSid = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('学号不能为空'));
+        }
+        setTimeout(() => {
+          if (!Number.isInteger(value)) {
+            callback(new Error('请输入数字值'));
+          } 
+          else {
+            callback();
+          }
+        }, 1000);
+      };
+      let validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } 
+        else {
+          if (this.ruleForm2.checkPass !== '') {
+            this.$refs.ruleForm2.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      let validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } 
+        else if (value !== this.ruleForm2.pass) {
+          callback(new Error('两次输入密码不一致!'));
+        } 
+        else {
+          callback();
+        }
+      };
+      let validatePhone = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('手机不能为空'));
+        }
+        setTimeout(() => {
+          if (!Number.isInteger(value)) {
+            callback(new Error('请输入数字值'));
+          } 
+          else {
+            value += ""
+            if (value.length != 11) {
+                console.log(value.length)
+              callback(new Error('手机号必定为11位'));
+            } else {
+              callback();
+            }
+          }
+        }, 1000);
+      };
+      let validateEmail = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入邮箱'));
+        }
+        else {
+          callback();
+        }
+      };
+
+      return {
+        ruleForm2: {
+            username: '',
+            sid: '',
+            pass: '',
+            checkPass: '',
+            phone: '',
+            email: '',
+        },
+        rules2: {
+            username: [
+                { validator: validateUsername, trigger: 'blur' }
+            ],
+            sid: [
+                { validator: validateSid, trigger: 'blur' }
+            ],
+            pass: [
+                { validator: validatePass, trigger: 'blur' }
+            ],
+            checkPass: [
+                { validator: validatePass2, trigger: 'blur' }
+            ],
+            phone: [
+                { validator: validatePhone, trigger: 'blur' }
+            ],
+            email: [
+                { validator: validateEmail, trigger: 'blur' }
+            ],
+        }
+      };
+  },
+
+  methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+              this.register()
+          } 
+          else {
+            console.log('error submit!!')
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      },
+      login() {
+          this.$router.push('/signin')
+      },
+
+      register() {
+          let url = '/api/newUser'
+          let params = '?id=' + this.ruleForm2.sid + 
+                        '&name=' + this.ruleForm2.username + 
+                        '&password=' + this.ruleForm2.pass + 
+                        '&mail=' + this.ruleForm2.email + 
+                        '&phone=' + this.ruleForm2.phone + 
+                        '&url='
+          this.$ajax
+            .get(url + params)
+            .then(res => {
+                if (res.data.ok) {
+                    this.$router.push({name: 'Personal', params: {uid: res.data.data.id}})
+                }
+                else {
+                    alert('fail to register')
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            })
+      }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
